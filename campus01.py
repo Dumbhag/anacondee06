d074ae956a10f4917da705a2cd5da34415d5fb2f
Dumbhag <s6519410057@sau.ac.th>
2023-09-27 11:54:43 +0700

    code.v1

---
+++ campus01.py
@@ -0,0 +1,12 @@
+# Control Flow / Control Statement
+# Condition / Selection
+# กระบวนการทำงานที่ต้องมีการพิสูจน์/ตรวจสอบ ก่อนที่จะทำงานใดๆ
+# if, if-else, if-else-if
+
+
+#Loop / Repeat / Iteration
+# กระบวนการทำงานที่มีการทำงานเดิมๆ ซ้ำ ซ้ำกี่รอบก็ได้แต่ต้องรู้จบ
+# คำสั่ง while, for
+
+# break
+# continue
---
+++ campus07.py
@@ -0,0 +1,14 @@
+#break
+for x in range(5) :
+    if x == 3 :
+        break   #ทำงานเมื่อไหร่ หยุดทำงานทันที
+    print('Crispy Pork Rice')
+
+print('++++++++++++++++')
+print('++++++++++++++++')
+
+#cantinue
+for y in range(5) :
+    if y == 3 :
+        continue  #ทำงานเมื่ไหร่ จบรอบนั้นไปรอบต่อไปทันที
+    print(f'Nomsod {y}')
